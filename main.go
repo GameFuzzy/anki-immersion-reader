GameFuzzy/anki-immersion-reader

commit 1fc589fbbed971d50f8c2a07183599dfcbb1a15b

Make deck a required argument

main.go
@@ -15,13 +15,8 @@ import (
 
 func main() {
 	// Retrieve arguments
-	if len(os.Args) < 2 {
-		panic(errors.New("Not enough arguments provided (expected ./anki-immersion-reader filepath [deck] [field]"))
-	}
-
-	deckName := "current"
-	if len(os.Args) > 2 {
-		deckName = os.Args[2]
+	if len(os.Args) < 3 {
+		panic(errors.New("Not enough arguments provided (expected ./anki-immersion-reader filepath deck [field]"))
 	}
 
 	fieldName := "Sentence"

commit 8d3c3f95a7b39262abf01f80e348f257b72b0c25

Fix build error caused by 1fc589fb

main.go
@@ -19,13 +19,16 @@ func main() {
 		panic(errors.New("Not enough arguments provided (expected ./anki-immersion-reader filepath deck [field]"))
 	}
 
+	filePath := os.Args[1]
+	deckName := os.Args[2]
+
 	fieldName := "Sentence"
 	if len(os.Args) > 3 {
 		fieldName = os.Args[3]
 	}
 
 	// Create map from words to sentences
-	wordSentenceMap, err := CreateWordSentenceMapFromAnkiDojoExport(os.Args[1])
+	wordSentenceMap, err := CreateWordSentenceMapFromAnkiDojoExport(filePath)
 
 	// Make array of just words
 	keys := make([]string, 0, len(wordSentenceMap))